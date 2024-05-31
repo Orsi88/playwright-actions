4d3fcdeb26fc414d37d368919af1159e387c6951
Júlia Martins Orsi <martinsorsi88@gmail.com>
2024-05-30 22:28:41 -0300

    adicionando tempo

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -3,36 +3,50 @@ import { test, expect, Page } from '@playwright/test';
 test('usuário obrigatório', async ({ page }) => {
   await login(page, '', 'senha123')
   await toast(page, 'Informe o seu nome de usuário!')
+
+  await page.waitForTimeout(3000)
 });
 
 test('senha obrigatória', async ({ page }) => {
   await login(page,'qa', '')
   await toast(page, 'Informe a sua senha secreta!')
+
+  await page.waitForTimeout(3000)
 })
 
 test('usuário não existe', async ({ page }) => {
   await login(page,'teste', 'teste')
   await toast(page, 'Oops! Credenciais inválidas :(')
+
+  await page.waitForTimeout(3000)
 })
 
 test('senha incorreta', async ({ page }) => {
   await login(page,'qa', 'teste')
   await toast(page, 'Oops! Credenciais inválidas :(')
+
+  await page.waitForTimeout(3000)
 })
 
 test('com sucesso', async ({ page }) => {
   await login(page,'qa', 'xperience')
   await modal(page, 'Suas credenciais são válidas :)')
+
+  await page.waitForTimeout(3000)
 })
 
 const toast = async (page: Page, message: string) => {
   const target = page.locator('div[role=status]')
   await expect(target).toHaveText(message);
+
+  await page.waitForTimeout(3000)
 }
 
 const modal = async (page: Page, message: string) => {
   const target = page.locator('.swal2-html-container')
   await expect(target).toHaveText(message);
+
+  await page.waitForTimeout(3000)
 }
 
 const login = async (page: Page, user: string, pass: string) => {
@@ -48,5 +62,7 @@ const login = async (page: Page, user: string, pass: string) => {
       ? await password.fill(pass) : null
 
     await page.click('css=button >> text=Entrar')
+
+    await page.waitForTimeout(3000)
 }
 
